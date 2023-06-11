955394fc33a408bf862f3d58844da76941124f39
boomsik <pozdnyakov151994@gmail.com>
2023-06-11 22:35:49 +0300

    add video files and test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+const STORAGE_KEY = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
